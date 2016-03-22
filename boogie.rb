fb16e82e6eccf132b95264716ab42a102f1cbba1
Arshavir Ter-Gabrielyan <arsh@technoworks.ru>
2016-03-22 20:13:26 +0100

    Important fixes for boogie.rb

--- boogie.rb
+++ boogie.rb
@@ -1,19 +1,26 @@
+require "formula"
 class Boogie < Formula
-  desc "Boogie is an Intermediate Verification Language (IVL) for describing proof obligations to be discharged by a reasoning engine, typically an SMT solver."
   homepage "http://boogie.codeplex.com"
   url "https://github.com/boogie-org/boogie.git"
-  sha256 "4d1a71e3bde406426df7adbc726c25486a75e7d234faa76d58929bd82f3f4a5c"
   version "2.3"
 
-  depends_on "mono"# => :build
+  #FIXME this formula installs a wrong version of mono;
+  # need to satisfy this dep manually: http://www.mono-project.com/download/
+  #
+  #depends_on "mono"
 
   def install
     system "curl", "-L", "-o", "nuget.exe", "https://nuget.org/nuget.exe"
-    system "mono", "nuget.exe", "restore", "./Source/Boogie.sln"
-    system "xbuild", "Source/Boogie.sln"
+    system "/usr/local/bin/mono", "nuget.exe", "restore", "./Source/Boogie.sln"
+    system "/usr/local/bin/xbuild", "Source/Boogie.sln"
     system "mkdir", "#{prefix}/Binaries"
-    system "cp", "-r", "Binaries/", "#{prefix}/Binaries"
-    system "echo '#!/bin/sh'$'\\n''mono\ #{prefix}/Binaries/Boogie.exe\ \"$@\"'$'\\n' > #{prefix}/Binaries/boogie"
-    system "chmod", "+x", "#{prefix}/Binaries/boogie"
+    prefix.install Dir["Binaries/*"]
+    system "echo '#!/bin/sh'$'\\n''mono\ #{prefix}/Boogie.exe\ \"$@\"'$'\\n' > boogie"
+    system "chmod", "+x", "boogie"
+    bin.install "boogie"
+  end
+
+  test do
+    system "boogie"
   end
 end
